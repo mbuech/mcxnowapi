19298b7a24bdb6558e3dfed636a27a4d44c9ccb2
Matthew Buechler <longbuech@gmail.com>
2013-10-18 01:46:58 -0400

    Delete whoa.py

--- whoa.py
+++
@@ -1,4 +0,0 @@
-x = open("hello.txt", "r").read()
-print x.split("BTC</tla><balavail>")[1].split("</baltotal><btctotal>")[0].split("<")[0]
-y = open("hello.txt", "r").read()
-print y.split("PPC</tla><balavail>")[1].split("</baltotal><btctotal>")[0].split("<")[0]	
